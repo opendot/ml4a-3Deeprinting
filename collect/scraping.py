e1c012a06a91a5d864c90a60ef8e168a5af4d99e
pieroit <piero.savastano@gmail.com>
2016-11-24 13:13:04 +0100

    scraping

--- collect/scraping.py
+++ collect/scraping.py
@@ -1,5 +1,7 @@
 from lxml import html
 import requests
+import wget
+import time
 
 # starts here
 if __name__ == '__main__':
@@ -13,10 +15,29 @@ if __name__ == '__main__':
 
     for q in queries:
         for p in range(pages):
+
+            print 'Query', q, 'page', p
+
             searchUrl  = 'http://www.thingiverse.com/search/page:' + str(p) + '?sort=makes&q=' + q + '&type=things'
 
             searchPage = requests.get(searchUrl)
             tree = html.fromstring(searchPage.content)
 
-            print searchUrl, tree.xpath('//div[@title="buyer-name"]/text()')
-            exit()
+            # get item links
+            objectURLs = tree.xpath('//a[@class="thing-img-wrapper"]/@href')
+
+            for objectURL in objectURLs:
+                fullObjectUrl = 'http://www.thingiverse.com' + objectURL
+
+                objPage = requests.get(fullObjectUrl)
+                objTree = html.fromstring(objPage.content)
+
+                #print objPage.text
+                print '\t\t Page', fullObjectUrl
+                downloadURL = objTree.xpath('//a[@class="track thing-file-download-link"]/@href')
+                if len(downloadURL) > 0:
+                    downloadURL     = downloadURL[0]    # only first (they are ordered by downloads)
+                    fullDownloadURL = 'http://www.thingiverse.com/' + downloadURL
+                    print '\t\t Download', fullDownloadURL
+
+                    wget.download(fullDownloadURL, 'data/' + downloadURL + '_' + str(time.time()) )
