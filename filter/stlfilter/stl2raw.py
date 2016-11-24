849bfc17291770da3ac1ad4715d6a79be063c0ee
Massimo Santini <massimo.santini@gmail.com>
2016-11-24 17:35:08 +0100

    Finished raw converter

--- filter/stlfilter/stl2raw.py
+++ filter/stlfilter/stl2raw.py
@@ -1,7 +1,8 @@
 from argparse import ArgumentParser
+from os.path import splitext
 from StringIO import StringIO
 
-from stl import read_binary_file, binary
+from stl import read_binary_file, read_ascii_file, binary
 
 from stlfilter import process
 
@@ -9,21 +10,26 @@ def convert(fq_in_path, fq_out_path):
     print 'reading: {}...'.format(fq_in_path),
     with open(fq_in_path) as f: data = f.read()
     if data.startswith('solid'):
-        with open(fq_out_path, 'w') as f: f.write(data)
-        print ' copied in: {}'.format(fq_out_path)
+        try:
+            solid = read_ascii_file(StringIO(data))
+        except binary.FormatError:
+            print ' FAILED to convert!'
     else:
         try:
             solid = read_binary_file(StringIO(data))
         except binary.FormatError:
             print ' FAILED to convert!'
-        else:
-            with open(fq_out_path, 'w') as f: solid.write_ascii(f)
-            print ' converted in: {}'.format(fq_out_path)
+    fq_out_base, ext = splitext(fq_out_path)
+    fq_out_path = fq_out_base + '.raw'
+    with open(fq_out_path, 'w') as f:
+        for facet in solid.facets:
+            f.write( ' '.join(' '.join(map(str,vertex)) for vertex in facet.vertices) + '\n' )
+        print ' converted in: {}'.format(fq_out_path)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--input-dir', '-i', help = 'The directory containing the source stl files.', default = '.')
-    parser.add_argument('--output-dir', '-o', help = 'The directory that will contain the ascii stl files.', default = 'ascii')
+    parser.add_argument('--output-dir', '-o', help = 'The directory that will contain the raw files.', default = 'ascii')
     args = parser.parse_args()
 
     process(args.input_dir, args.output_dir, convert, '*.stl')
