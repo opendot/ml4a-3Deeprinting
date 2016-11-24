299c122c9851606bb3f7a44038868e88908cb98f
Massimo Santini <massimo.santini@gmail.com>
2016-11-24 15:35:12 +0100

    Fixed params and added autodetection of binary/ascii

--- filter/binary2ascii.py
+++ filter/binary2ascii.py
@@ -3,8 +3,9 @@ from errno import EEXIST, ENOENT
 from fnmatch import fnmatch
 from os import chmod, makedirs, walk
 from os.path import join, expandvars, expanduser, isdir, abspath, dirname, sep
+from StringIO import StringIO
 
-from stl import read_binary_file
+from stl import read_binary_file, binary
 
 def safe_makedirs(path, mode = 0700):
     try:
@@ -21,12 +22,23 @@ if __name__ == '__main__':
 
     base_dir = dirname(args.input_dir + sep)
     prefix_len = len(base_dir) + 1
-    for dirpath, dirnames, filenames in walk(base_dir):
+    for dirpath, dirnames, filenames in walk(base_dir, topdown = False):
         for path in filenames:
             fq_in_path = join(dirpath, path)
             if fnmatch(fq_in_path,'*.stl'):
                 dest_dir = join(args.output_dir, dirpath[prefix_len:])
                 safe_makedirs(dest_dir)
                 fq_out_path = join(dest_dir, path)
-                with open(fq_in_path) as f: solid = read_binary_file(f)
-                with open(fq_out_path, 'w') as f: solid.write_ascii(f)
+                print 'reading: {}...'.format(fq_in_path),
+                with open(fq_in_path) as f: data = f.read()
+                if data.startswith('solid'):
+                    with open(fq_out_path, 'w') as f: f.write(data)
+                    print ' copied in: {}'.format(fq_out_path)
+                else:
+                    try:
+                        solid = read_binary_file(StringIO(data))
+                    except binary.FormatError:
+                        print ' FAILED to convert!'
+                    else:
+                        with open(fq_out_path, 'w') as f: solid.write_ascii(f)
+                        print ' converted in: {}'.format(fq_out_path)
--- generate/train.sh
+++ generate/train.sh
@@ -2,10 +2,15 @@
 
 input="$1"
 if [ -z "$input" ]; then
-    echo train: specify the stl filename
+    echo "train: specify the stl filename"
     exit 1
 fi
 base_dir=$(realpath $(dirname "$input"))
 base_name=$(basename "$input" .stl)
+checkpoint_every="$2"
+if [ -z "$checkpoint_every" ]; then
+    checkpoint_every=1000
+    echo "train: checkpoining every $checkpoint_every iterations"
+fi
 
-exec docker run --rm -it -v "$base_dir":/data crisbal/torch-rnn:base th train.lua -input_h5 "/data/${base_name}.h5" -input_json "/data/${base_name}.json" -gpu -1 -checkpoint_every 100 -checkpoint_name "/data/${base_name}-checkpoint"
+exec docker run --rm -it -v "$base_dir":/data crisbal/torch-rnn:base th train.lua -input_h5 "/data/${base_name}.h5" -input_json "/data/${base_name}.json" -gpu -1 -checkpoint_every "$checkpoint_every" -checkpoint_name "/data/${base_name}-checkpoint"
