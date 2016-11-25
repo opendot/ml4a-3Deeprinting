8fc8417e4bf015d4a5c173c789912783115cc984
pieroit <piero.savastano@gmail.com>
2016-11-25 16:32:57 +0100

    4 floats network

--- generate/LSTM/lstm_network_for_regression_vector.py
+++ generate/LSTM/lstm_network_for_regression_vector.py
@@ -8,14 +8,17 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+
+
 # convert an array of values into a dataset matrix
 def create_dataset(dataset, look_back=1):
-	dataX, dataY = [], []
-	for i in range(len(dataset)-look_back-1):
-		a = dataset[i:(i+look_back), 0]
-		dataX.append(a)
-		dataY.append(dataset[i + look_back, 0])
-	return numpy.array(dataX), numpy.array(dataY)
+    dataX = []
+    dataY = []
+    for i in range( len(dataset) - look_back ):
+        dataX.append( dataset[i] )
+        dataY.append( dataset[i+look_back] )
+    return numpy.array(dataX), numpy.array(dataY)
+
 # fix random seed for reproducibility
 numpy.random.seed(7)
 # load the dataset
@@ -31,49 +34,68 @@ dataset = dataset.astype('float32')
 scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
 # split into train and test sets
-train_size = int(len(dataset) * 0.3)
+
+train_size = int(len(dataset) * 0.07) * 4 # 30% and divisible by 4
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+# reshape in blocks [x,y,z,e] train and test
+train = train.reshape( (int(len(train)/4.0), 4) )
+test  = test.reshape( (int(len(test)/4.0), 4) )
+
 # reshape into X=t and Y=t+1
-look_back = 4
+look_back = 1
 trainX, trainY = create_dataset(train, look_back)
-testX, testY = create_dataset(test, look_back)
+testX, testY   = create_dataset(test, look_back)
+
 # reshape input to be [samples, time steps, features]
 trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
 # create and fit the LSTM network
 model = Sequential()
-model.add(LSTM(4, input_dim=look_back))
-model.add(Dense(1))
+model.add(LSTM(4, input_dim=4))
+model.add(Dense(4))
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, nb_epoch=5, batch_size=1, verbose=2)
+model.fit(trainX, trainY, nb_epoch=200, batch_size=100, verbose=2)
+
 # make predictions
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
+
+# reshape vectors
+trainPredict = trainPredict.flatten()
+testPredict  = testPredict.flatten()
+trainY       = trainY.flatten()
+testY        = testY.flatten()
+
 # invert predictions
 trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform([trainY])
+trainY = scaler.inverse_transform(trainY)
 testPredict = scaler.inverse_transform(testPredict)
-testY = scaler.inverse_transform([testY])
-# calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+testY = scaler.inverse_transform(testY)
+
+# calculate root mean squared error]
+trainScore = math.sqrt(mean_squared_error(trainY, trainPredict))
 print('Train Score: %.2f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+testScore = math.sqrt(mean_squared_error(testY, testPredict))
 print('Test Score: %.2f RMSE' % (testScore))
+
 # shift train predictions for plotting
-trainPredictPlot = numpy.empty_like(dataset)
-trainPredictPlot[:, :] = numpy.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+#trainPredictPlot = numpy.empty_like(dataset)
+#trainPredictPlot[:, :] = numpy.nan
+#trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
 # shift test predictions for plotting
-testPredictPlot = numpy.empty_like(dataset)
-testPredictPlot[:, :] = numpy.nan
-testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+#testPredictPlot = numpy.empty_like(dataset)
+#testPredictPlot[:, :] = numpy.nan
+#testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
 # plot baseline and predictions
-plt.plot(scaler.inverse_transform(dataset))
-plt.plot(numpy.concatenate((trainPredict,testPredict),axis=0))
+#plt.plot(scaler.inverse_transform(dataset))
+#plt.plot(numpy.concatenate((trainPredict,testPredict),axis=0))
 #plt.plot(trainPredictPlot)
 #plt.plot(testPredictPlot)
 
-numpy.savetxt('output_dataset_ALL.txt', numpy.concatenate((trainPredict,testPredict),axis=0), fmt="%10f")
+numpy.savetxt('output_dataset_ALL_vector.txt', numpy.concatenate((trainPredict,testPredict),axis=0), fmt="%10f")
 
 plt.show()
