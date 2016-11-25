79691b7b082ea0f6f37a05eeb1acf5544864531d
Alessandro <alessandro.zomparelli@gmail.com>
2016-11-25 13:42:48 +0100

    new gcode

--- generate/LSTM/list_to_gcode_conversion.py
+++ generate/LSTM/list_to_gcode_conversion.py
@@ -4,10 +4,10 @@ out = open('output_gcode.gcode', 'w')
 
 for j in range(len(data)/4):
     i = j*4
-    x = data[i][:-2].strip()
-    y = data[i+1][:-2].strip()
-    z = data[i+2][:-2].strip()
-    e = data[i+3][:-2].strip()
+    x = data[i].strip()
+    y = data[i+1].strip()
+    z = data[i+2].strip()
+    e = data[i+3].strip()
     out.write('G1 X' + x[:-2] + ' Y' + y[:-2] + ' Z' + z[:-2] + ' E' + e + '\n')
 
 out.close()
